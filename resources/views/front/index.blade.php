016a8951d392caaafb8e9748ca0e3f8535225383
刘义 <forehalo@gmail.com>
2016-01-09 17:11:04 +0800

    pagination

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -26,6 +26,15 @@
             <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
         </li>
     </ul>
+    <ul class="pagination">
+        <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+        <li class="active"><a href="#!">1</a></li>
+        <li class="waves-effect"><a href="#!">2</a></li>
+        <li class="waves-effect"><a href="#!">3</a></li>
+        <li class="waves-effect"><a href="#!">4</a></li>
+        <li class="waves-effect"><a href="#!">5</a></li>
+        <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+    </ul>
 @stop
 
 @section('script')
