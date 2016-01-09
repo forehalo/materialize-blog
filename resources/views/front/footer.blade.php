d8f04d2534b3c72a79fa4b50e729144d8e3c6b8d
刘义 <forehalo@gmail.com>
2016-01-09 17:11:09 +0800

    footer

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,13 +1,35 @@
 <footer class="page-footer blue accent-2">
     <div class="container">
         <div class="row">
-
+            <div class="col l3 offset-l3 s6 grey-text text-lighten-2">
+                <h5>Pages</h5>
+                <ul>
+                    <li>About Me</li>
+                    <li>Archive</li>
+                    <li>Categories</li>
+                    <li>Tags</li>
+                </ul>
+            </div>
+            <div class="col l3 s6 grey-text text-lighten-2">
+                <h5>Feed</h5>
+                <ul>
+                    <li>Atom Feed</li>
+                    <li>RSS Feed</li>
+                </ul>
+            </div>
+            <div class="col l3 s6 grey-text text-lighten-2">
+                <h5>About Me</h5>
+                <ul>
+                    <li>e-mail</li>
+                    <li>Github</li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class="footer-copyright">
         <div class="container">
             Copyright © 2015-2016 forehalo
-            <a class="grey-text text-lighten-4 right" href="">Hello World</a>
+            <a class="grey-text text-lighten-4 right" href="">// Forehalo</a>
         </div>
     </div>
 </footer>
