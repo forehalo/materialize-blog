66041cce7e6b1c4ad5f30fd87b53e3026af91eac
刘义 <forehalo@gmail.com>
2016-01-12 21:56:50 +0800

    footer update

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,35 +1,8 @@
-<footer class="page-footer blue accent-2">
-    <div class="container">
-        <div class="row">
-            <div class="col l3 offset-l3 s6 grey-text text-lighten-2">
-                <h5>Pages</h5>
-                <ul>
-                    <li>About Me</li>
-                    <li>Archive</li>
-                    <li>Categories</li>
-                    <li>Tags</li>
-                </ul>
-            </div>
-            <div class="col l3 s6 grey-text text-lighten-2">
-                <h5>Feed</h5>
-                <ul>
-                    <li>Atom Feed</li>
-                    <li>RSS Feed</li>
-                </ul>
-            </div>
-            <div class="col l3 s6 grey-text text-lighten-2">
-                <h5>About Me</h5>
-                <ul>
-                    <li>e-mail</li>
-                    <li>Github</li>
-                </ul>
-            </div>
-        </div>
-    </div>
+<footer class="page-footer transparent">
     <div class="footer-copyright">
-        <div class="container">
+        <div class="container black-text text-lighten-5">
             Copyright © 2015-2016 forehalo
-            <a class="grey-text text-lighten-4 right" href="">// Forehalo</a>
+            <a class="right black-text text-lighten-5" href="">// Forehalo</a>
         </div>
     </div>
 </footer>
