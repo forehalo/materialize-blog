22e5766c8a53e208db8ddbb7d6ac5e8ba15b3791
刘义 <forehalo@gmail.com>
2016-01-24 21:42:35 +0800

    update layout

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -10,40 +10,52 @@
     <div class="divider"></div>
     <ul class="collapsible popout" data-collapsible="accordion" id="article">
         <li>
-            <div class="collapsible-header" id="1"><i class="material-icons">filter_drama</i><h4 id="title">laravel5 配合
-                    Queues 使用 Artisan
-                    Commands</h4>
-
+            <div class="collapsible-header" id="1">
+                <h5 id="title">laravel5 配合 Queues 使用 Artisan Commands
+                </h5>
+                <div class="label">published at 2016-01-24  <i class="material-icons">visibility</i>10<i class="material-icons">comment</i>10</div>
                 <p class="intro">
-                    Laravel本身给我们提供了许多强大的Artisan命令，但这些都是基于框架的一些操作，有时我们也需要自己定制一些命令。接下来就介绍一下如何添加自己的命令，以及使用队列来执行命令。<i
-                            class="material-icons tiny icon-demo">insert_chart</i></p>
+                    Laravel本身给我们提供了许多强大的Artisan命令，但这些都是基于框架的一些操作，有时我们也需要自己定制一些命令。接下来就介绍一下如何添加自己的命令，以及使用队列来执行命令。
+                </p>
+                <div class="row">
+                    <div class="col m12 l12">
+                        <a href="#" class="btn waves-effect">Email</a>
+                        <a href="#" class="btn waves-effect">Gi</a>
+                        <a href="#" class="btn waves-effect">Email</a>
+                    </div>
+                </div>
             </div>
+
             <div class="collapsible-body markdown-body">
                 <p>Laravel本身给我们提供了许多强大的Artisan 命令，但这些都是基于框架的一些操作，有时我们也需要自己定制一些命令。接下来就介绍一下如何添加自己的命令，以及使用队列来执行命令。</p>
                 <pre class=" language-php"><code class=" language-php"> <span class="token string">'enable'</span> <span
                                 class="token operator">=</span><span class="token operator">&gt;</span> <span
                                 class="token punctuation">[</span>
        <span class="token comment" spellcheck="true"> //被使用概率为2/3
-</span> <span class="token string">'Luosimao'</span> <span class="token operator">=</span><span class="token operator">&gt;</span><span class="token string">'20'</span><span class="token punctuation">,</span>
+</span> <span class="token string">'Luosimao'</span> <span class="token operator">=</span><span class="token operator">&gt;</span><span
+                                class="token string">'20'</span><span class="token punctuation">,</span>
 
        <span class="token comment" spellcheck="true"> //被使用概率为1/3，且为备用代理器
-</span> <span class="token string">'YunPian'</span> <span class="token operator">=</span><span class="token operator">&gt;</span><span class="token string">'10 backup'</span><span class="token punctuation">,</span>
+</span> <span class="token string">'YunPian'</span> <span class="token operator">=</span><span class="token operator">&gt;</span><span
+                                class="token string">'10 backup'</span><span class="token punctuation">,</span>
 
        <span class="token comment" spellcheck="true"> //仅为备用代理器
 </span> <span class="token string">'YunTongXun'</span> <span class="token operator">=</span><span
                                 class="token operator">&gt;</span> <span class="token string">'0 backup'</span><span
                                 class="token punctuation">,</span>
-<span class="token punctuation">]</span><span class="token punctuation">;</span></code></pre>
+                        <span class="token punctuation">]</span><span class="token punctuation">;</span></code></pre>
             </div>
         </li>
         <li>
-            <div class="collapsible-header" id="2"><i class="material-icons">place</i><h4>foreach 循环中注意事项</h4><p class="intro">
+            <div class="collapsible-header" id="2"><i class="material-icons">place</i><h5>foreach 循环中注意事项</h5>
+                <p class="intro">
                     Laravel本身给我们提供了许多强大的Artisan命令，但这些都是基于框架的一些操作，有时我们也需要自己定制一些命令。接下来就介绍一下如何添加自己的命令，以及使用队列来执行命令。</p>
             </div>
             <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
         </li>
         <li>
-            <div class="collapsible-header" id="3"><i class="material-icons">whatshot</i>Excel-Worker 一个 PHP 操作 excel 包
+            <div class="collapsible-header" id="3"><i class="material-icons">whatshot</i> <h5>Excel-Worker 一个 PHP 操作
+                    excel 包</h5>
                 <p class="intro">
                     Laravel本身给我们提供了许多强大的Artisan命令，但这些都是基于框架的一些操作，有时我们也需要自己定制一些命令。接下来就介绍一下如何添加自己的命令，以及使用队列来执行命令。</p>
             </div>
