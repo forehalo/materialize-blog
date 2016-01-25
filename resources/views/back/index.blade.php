b244a702ad53056670c0070e4d18b93137c38d02
刘义 <forehalo@gmail.com>
2016-01-25 22:46:16 +0800

    dashboard style

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -4,6 +4,65 @@
 @stop
 
 @section('main')
-
+    <div class="container">
+        <h4>Dashboard</h4>
+        <div class="divider"></div>
+        <div class="row white-text dashboard">
+            <div class="col s12 m6 l4">
+                <div class="card-panel blue">
+                    <div class="card-content">
+                        <i class="material-icons medium">create</i>
+                        <div class="right">
+                            <div class="number">2</div>
+                            <div>Total Posts</div>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                    <a href="#">
+                        <div class="card-action">
+                            <div class="left">View</div>
+                            <i class="material-icons right">arrow forward</i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col s12 m6 l4">
+                <div class="card-panel teal">
+                    <div class="card-content">
+                        <i class="material-icons medium">comment</i>
+                        <div class="right">
+                            <div class="number">2</div>
+                            <div>New Comments</div>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                    <a href="#">
+                        <div class="card-action">
+                            <div class="left">View</div>
+                            <i class="material-icons right">arrow forward</i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col s12 m6 l4">
+                <div class="card-panel pink">
+                    <div class="card-content">
+                        <i class="material-icons medium">email</i>
+                        <div class="right">
+                            <div class="number">2</div>
+                            <div>New Messages</div>
+                        </div>
+                    </div>
+                    <div class="divider"></div>
+                    <a href="#">
+                        <div class="card-action">
+                            <div class="left">View</div>
+                            <i class="material-icons right">arrow forward</i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
