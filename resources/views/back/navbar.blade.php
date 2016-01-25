a5e58a433d3f220f10c579a5932f29d84caafd81
刘义 <forehalo@gmail.com>
2016-01-25 22:46:27 +0800

    navbar

--- resources/views/back/navbar.blade.php
+++ resources/views/back/navbar.blade.php
@@ -1,17 +1,14 @@
-<div class="navbar-fixed">
+<div class="navbar-fixed z-depth-2">
     <nav>
         <div class="nav-wrapper">
             <a href="#" data-activates="nav-mobile"
                class="button-collapse btn-flat pink-text"><i
                         class="material-icons">menu</i></a>
-            <a href="#" class="brand-logo pink-text">个人博客管理 <label for="">by Forehalo</label></a>
+            <a href="javascript:void(0);" class="brand-logo pink-text">个人博客管理 <label for="">by Forehalo</label></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="#" class=" waves-effect waves-light"><i class="material-icons">settings</i></a></li>
+            </ul>
         </div>
     </nav>
 
-    <ul class="side-nav fixed" id="nav-mobile">
-        <li><a href="#">Sass</a></li>
-        <li><a href="#" class="active">Components</a></li>
-        <li><a href="#">Javascript</a></li>
-        <li><a href="#">Mobile</a></li>
-    </ul>
 </div>
