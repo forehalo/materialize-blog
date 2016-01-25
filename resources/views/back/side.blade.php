ea7dfd8ab5027f87e8d337885ecf6ef98af40d4a
刘义 <forehalo@gmail.com>
2016-01-25 22:47:10 +0800

    update side navigation

---
+++ resources/views/back/side.blade.php
@@ -0,0 +1,16 @@
+<ul class="side-nav fixed z-depth-4" id="nav-mobile">
+    <li><a href="#">Dashboard<i class="material-icons left">dashboard</i></a></li>
+    <ul class="collapsible">
+        <li><a class="collapsible-header">Posts <i class="material-icons left">create</i><i class="material-icons right">&#xE315;</i></a>
+            <div class="collapsible-body">
+                <ul>
+                    <li><a href="#">All</a></li>
+                    <li><a href="#">New</a></li>
+                </ul>
+            </div>
+        </li>
+    </ul>
+    <li><a href="#">Comments<i class="material-icons left">comment</i></a></li>
+    <li class="active"><a href="#" class="active">Messages<i class="material-icons left">mail</i></a></li>
+    <li><a href="#">Settings<i class="material-icons left">settings</i></a></li>
+</ul>
