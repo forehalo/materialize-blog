c0a8abfddf44cfbff0c6dd3c1f2789650cfdd1c2
刘义 <forehalo@gmail.com>
2016-01-26 22:10:46 +0800

    side navigation update

--- resources/views/back/side.blade.php
+++ resources/views/back/side.blade.php
@@ -1,16 +1,16 @@
-<ul class="side-nav fixed z-depth-4" id="nav-mobile">
+<ul class="side-nav fixed z-depth-4" id="nav-mobile" style="width: 240px">
     <li><a href="#">Dashboard<i class="material-icons left">dashboard</i></a></li>
-    <ul class="collapsible">
+    <ul class="collapsible collapsible-accordion">
         <li><a class="collapsible-header">Posts <i class="material-icons left">create</i><i class="material-icons right">&#xE315;</i></a>
             <div class="collapsible-body">
                 <ul>
-                    <li><a href="#">All</a></li>
+                    <li><a href="{!! url('blog') !!}">All</a></li>
                     <li><a href="#">New</a></li>
                 </ul>
             </div>
         </li>
     </ul>
     <li><a href="#">Comments<i class="material-icons left">comment</i></a></li>
-    <li class="active"><a href="#" class="active">Messages<i class="material-icons left">mail</i></a></li>
+    <li class="active"><a href="#" class="waves-effect">Messages<i class="material-icons left">mail</i></a></li>
     <li><a href="#">Settings<i class="material-icons left">settings</i></a></li>
 </ul>
