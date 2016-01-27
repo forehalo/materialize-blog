5a66465791715b59314c0afa956772244c8b24e8
刘义 <forehalo@gmail.com>
2016-01-27 20:16:56 +0800

    navbar active

--- resources/views/front/navbar.blade.php
+++ resources/views/front/navbar.blade.php
@@ -1,3 +1,4 @@
+@inject('cates', 'App\Models\Category')
 <div class="navbar-fixed">
     <nav class="blue accent-2">
         <div class="container">
@@ -11,27 +12,26 @@
                     <li><a href="/" class="waves-effect waves-block"><i
                                     class="material-icons left">list</i>Posts</a>
                     </li>
-                    <li><a href="#" class="waves-effect waves-block dropdown-button" data-activates="cate_drop">Categories<i
+                    <li><a href="#" class="waves-effect waves-block dropdown-button" data-activates="cate_drop_top">Categories<i
                                     class="material-icons left">class</i><i
                                     class="material-icons right">arrow_drop_down</i></a>
-                        <ul id="cate_drop" class="dropdown-content">
-                            <li class="divider"></li>
-                            <li><a href="#">cate1</a></li>
-                            <li><a href="#">cate2</a></li>
-                            <li><a href="#">cate3</a></li>
+                        <ul id="cate_drop_top" class="dropdown-content">
+                            @foreach($cates::orderBy('hot', 'desc')->get() as $cate)
+                                <li><a href="{!! url('/cate/' . $cate->name) !!}">{!! $cate->name !!}</a></li>
+                            @endforeach
                         </ul>
                     </li>
                     <li><a href="#" class="waves-effect waves-block dropdown-button" data-activates="archive_drop">Archives<i
                                     class="material-icons left">label_outline</i><i class="material-icons right">arrow_drop_down</i></a>
                         <ul id="archive_drop" class="dropdown-content">
-                            <li><a href="#" class="black-text">Group By</a></li>
+                            <li><a href="#" class="black-text disabled">Group By</a></li>
                             <li class="divider"></li>
-                            <li><a href="#">Categories</a></li>
-                            <li><a href="#">Time</a></li>
-                            <li><a href="#">Tags</a></li>
+                            <li><a href="{!! url('archive/cates') !!}">Categories</a></li>
+                            <li><a href="{!! url('archive/time') !!}">Time</a></li>
+                            <li><a href="{!! url('archive/tags') !!}">Tags</a></li>
                         </ul>
                     </li>
-                    <li><a href="#" class="waves-effect waves-block">About<i
+                    <li><a href="{!! url('about') !!}" class="waves-effect waves-block">About<i
                                     class="material-icons left">perm_identity</i></a></li>
                     <li>
                         <a class=""><i class="material-icons">search</i></a>
@@ -39,9 +39,17 @@
                 </ul>
                 <ul id="nav-mobile" class="side-nav blue accent-3">
                     <li><a href="#" class="waves-effect waves-teal white-text">Posts<i class="material-icons left">list</i></a></li>
-                    <li><a href="#" class="waves-effect waves-teal white-text">Categories <i class="material-icons left">class</i></a></li>
-                    <li><a href="#" class="waves-effect waves-teal white-text">Tags <i class="material-icons left">label_outline</i></a></li>
-                    <li><a href="#" class="waves-effect waves-teal white-text">About <i class="material-icons left">perm_identity</i></a></li>
+                    <li><a href="#" class="waves-effect waves-block dropdown-button white-text" data-activates="cate_drop_side">Categories<i
+                                    class="material-icons left">class</i><i
+                                    class="material-icons right">arrow_drop_down</i></a>
+                        <ul id="cate_drop_side" class="dropdown-content">
+                            @foreach($cates::orderBy('hot', 'desc')->get() as $cate)
+                                <li><a href="{!! url('/cate/' . $cate->name) !!}">{!! $cate->name !!}</a></li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    <li><a href="{!! url('archive/tags') !!}" class="waves-effect waves-teal white-text">Tags <i class="material-icons left">label_outline</i></a></li>
+                    <li><a href="{!! url('about') !!}" class="waves-effect waves-teal white-text">About <i class="material-icons left">perm_identity</i></a></li>
                     <li>
                         <div class="input-field">
                             <input id="search" type="search" required>
