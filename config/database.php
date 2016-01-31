6279753f91e1d71bc9bb9744a3f2b3517329ab37
刘义 <forehalo@gmail.com>
2016-01-31 14:18:44 +0800

    save session in redis

--- config/database.php
+++ config/database.php
@@ -116,9 +116,8 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => env('REDIS_HOST', 'localhost'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port'     => env('REDIS_PORT', 6379),
+            'host'     => '127.0.0.1',
+            'port'     => 6379,
             'database' => 0,
         ],
 
