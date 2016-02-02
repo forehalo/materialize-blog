70a6ae2de3ee5e59641904bdb54794503870de8f
刘义 <forehalo@gmail.com>
2016-02-02 20:59:50 +0800

    normal index using collection component

--- resources/views/front/posts/normalIndex.blade.php
+++ resources/views/front/posts/normalIndex.blade.php
@@ -1,1 +1,38 @@
-{!! dd($posts) !!}
+@extends('front.template')
+@section('title')
+    <title>{!! config('blog.title') !!}</title>
+@stop
+
+@section('main')
+    <h5>共 {!! $posts->total() !!} 篇</h5>
+    <div class="divider"></div>
+    <div class="collection z-depth-3">
+        @foreach($posts as $post)
+            <div class="collection-item">
+                <a href="{!! url('/posts/' . $post->slug) !!}"><h5>{!! $post->title !!}</h5></a>
+                <div class="label">published at {!! substr($post->created_at, 0, 10) !!}
+                    <i class="material-icons">visibility</i>{!! $post->view_count !!}
+                    <i class="material-icons">comment</i>{!! $post->comment_count !!}
+                </div>
+                <p class="summary">{!! $post->summary !!}</p>
+                <div class="row">
+                    <div class="col s12 m12 l12">
+                        @foreach($post->tags as $tag)
+                            <a href="{!! url('/tags/' . $tag->id) !!}" class="chip">{!! $tag->name !!}</a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    {!! $posts->links() !!}
+@stop
+
+@section('script')
+    <script>
+        $('nav').addClass('blue');
+        $('.top-tags > a').addClass('blue');
+        $('.side-nav').addClass('blue');
+        $('.collapsible-according').addClass('blue');
+    </script>
+@stop
