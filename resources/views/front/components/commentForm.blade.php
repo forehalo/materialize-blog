9a2b00190c49703a71891ccfaf9e8e84f73692c1
刘义 <forehalo@gmail.com>
2016-02-02 15:53:22 +0800

    enable validation and save old input

--- resources/views/front/components/commentForm.blade.php
+++ resources/views/front/components/commentForm.blade.php
@@ -1,21 +1,23 @@
 <div class="divider"></div>
 <h5>Make a Comment</h5>
-{!! Form::open(['url' => url('/posts/' . $id . '/'), 'class' => 'col s12', 'id' => 'comment-form']) !!}
+
+{!! Form::open(['url' => url('comments'), 'class' => 'col s12', 'id' => 'comment-form']) !!}
+{!! Form::hidden('post_id', $id) !!}
 {!! Form::hidden('parent', 0) !!}
-<div class="input-field col s6 l4">
-    <input id="name" type="text" class="validate">
-    <label for="name"><i class="material-icons">perm_identity</i> Name *</label>
+<div class="input-field col s12 l4">
+    {!! Form::text('name', old('name'), ['id' => 'name', 'class' => $errors->comment->first('name') ? 'validate invalid' : 'validate' ]) !!}
+    <label for="name"><i class="material-icons">perm_identity</i> Name</label>
 </div>
-<div class="input-field col s6 l4">
-    <input id="email" type="text" class="validate">
-    <label for="email"><i class="material-icons">email</i> Email(hide)</label>
+<div class="input-field col s12 l4">
+    {!! Form::email('email', old('email'), ['id' => 'email', 'class' => $errors->comment->first('email') ? 'validate invalid' : 'validate' ]) !!}
+    <label for="email"><i class="material-icons">email</i> Email(invisible)</label>
 </div>
-<div class="input-field col s6 l4">
-    <input id="blog" type="text" class="validate">
-    <label for="blog"><i class="material-icons">web</i> Website</label>
+<div class="input-field col s12 l4">
+    {!! Form::url('blog', old('blog'), ['id' => 'blog', 'class' => $errors->comment->first('blog') ? 'validate invalid' : 'validate' ]) !!}
+    <label for="blog"><i class="material-icons">web</i> Website(http://...)</label>
 </div>
 <div class="input-field col s12">
-    <textarea id="content" class="materialize-textarea"></textarea>
+    {!! Form::textarea('content', old('content'), ['id' => 'content', 'class' => 'materialize-textarea']) !!}
     <label for="content"><i class="material-icons">comment</i>content</label>
 </div>
 <div class="input-field col s12">
