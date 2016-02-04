3ab4350dd64e1adea76e7e9bab4a1ed88ab6983a
刘义 <forehalo@gmail.com>
2016-02-04 20:39:13 +0800

    make Admin model user provider

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class Admin extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
+    use Authenticatable, CanResetPassword;
+
     protected $table = 'admin';
+
+    protected $hidden = ['password', 'remember_token'];
+
 }
