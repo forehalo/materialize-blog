5bc1f624c00ea6bbb82455fb61347323fe1c4f06
刘义 <forehalo@gmail.com>
2016-02-05 21:38:50 +0800

    active path

--- app/helpers.php
+++ app/helpers.php
@@ -21,3 +21,22 @@ if (!function_exists('intToMonth')) {
         return $monthArr[$i];
     }
 }
+
+if (!function_exists('classActivePath')) {
+    function classActivePath($path){
+        return Request::is($path) ? 'active' : '';
+    }
+}
+
+if (!function_exists('classActiveSegment')) {
+    function classActiveSegment($segment, $value)
+    {
+        if(!is_array($value)) {
+            return Request::segment($segment) == $value ? 'active' : '';
+        }
+        foreach ($value as $v) {
+            if(Request::segment($segment) == $v) return 'active';
+        }
+        return '';
+    }
+}
--- resources/views/back/components/side.blade.php
+++ resources/views/back/components/side.blade.php
@@ -1,16 +1,20 @@
 <ul class="side-nav fixed z-depth-4" id="nav-mobile" style="width: 240px">
-    <li><a href="#">Dashboard<i class="material-icons left">dashboard</i></a></li>
-    <ul class="collapsible collapsible-accordion">
-        <li><a class="collapsible-header">Posts <i class="material-icons left">create</i><i class="material-icons right">&#xE315;</i></a>
-            <div class="collapsible-body">
-                <ul>
-                    <li><a href="{!! url('blog') !!}">All</a></li>
-                    <li><a href="#">New</a></li>
-                </ul>
-            </div>
-        </li>
-    </ul>
-    <li><a href="#">Comments<i class="material-icons left">comment</i></a></li>
-    <li class="active"><a href="#" class="waves-effect">Messages<i class="material-icons left">mail</i></a></li>
-    <li><a href="#">Settings<i class="material-icons left">settings</i></a></li>
+    <li class="bold {!! classActivePath('dashboard') !!}"><a href="#">Dashboard<i class="material-icons left">dashboard</i></a></li>
+    <li class="no-padding {!! classActiveSegment(1, 'posts') !!}">
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header">Posts <i class="material-icons left">create</i><i
+                            class="material-icons right">&#xE315;</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{!! url('blog') !!}">All</a></li>
+                        <li><a href="{!! url('posts/create') !!}">New</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="bold {!! classActivePath('comments') !!}"><a href="#" class="waves-effect">Comments<i class="material-icons left">comment</i></a></li>
+    <li class="bold {!! classActivePath('messages') !!}"><a href="#" class="waves-effect">Messages<i class="material-icons left">mail</i></a></li>
+    <li class="bold {!! classActivePath('settings') !!}"><a href="#" class="waves-effect">Settings<i class="material-icons left">settings</i></a></li>
 </ul>
