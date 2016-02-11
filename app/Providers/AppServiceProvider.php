6a713cec6ab105a2d2058ed594266488aed0adf6
刘义 <forehalo@gmail.com>
2016-02-11 19:29:58 +0800

    add tags input validation into validate service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Validation;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::resolver(function($translator, $data, $rules, $messages)
+        {
+            return new Validation($translator, $data, $rules, $messages);
+        });
     }
 
     /**
---
+++ app/Services/Validation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Validation\Validator;
+
+class Validation extends Validator
+{
+    public function validateTags($attribute, $value, $parameters)
+    {
+        return preg_match("/^[A-Za-z0-9-éèàù]{1,50}?(,[A-Za-z0-9-éèàù]{1,50})*$/", $value);
+    }
+}
