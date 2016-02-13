e3a36dae698d0b8e2106b43134c27a95d26ce416
刘义 <forehalo@gmail.com>
2016-02-13 17:41:23 +0800

    new helper functions
    dom attributes generator & checkbox generator

--- app/helpers.php
+++ app/helpers.php
@@ -4,15 +4,15 @@ if (!function_exists('intToMonth')) {
     function intToMonth($i)
     {
         $monthArr = [
-            1  => "January",
-            2  => "February",
-            3  => "March",
-            4  => "April",
-            5  => "May",
-            6  => "June",
-            7  => "July",
-            8  => "August",
-            9  => "September",
+            1 => "January",
+            2 => "February",
+            3 => "March",
+            4 => "April",
+            5 => "May",
+            6 => "June",
+            7 => "July",
+            8 => "August",
+            9 => "September",
             10 => "October",
             11 => "November",
             12 => "December"
@@ -23,7 +23,8 @@ if (!function_exists('intToMonth')) {
 }
 
 if (!function_exists('classActivePath')) {
-    function classActivePath($path){
+    function classActivePath($path)
+    {
         return Request::is($path) ? 'active' : '';
     }
 }
@@ -31,20 +32,36 @@ if (!function_exists('classActivePath')) {
 if (!function_exists('classActiveSegment')) {
     function classActiveSegment($segment, $value)
     {
-        if(!is_array($value)) {
+        if (!is_array($value)) {
             return Request::segment($segment) == $value ? 'active' : '';
         }
         foreach ($value as $v) {
-            if(Request::segment($segment) == $v) return 'active';
+            if (Request::segment($segment) == $v) return 'active';
         }
         return '';
     }
 }
 
-if (!function_exists('publishedCheckbox')) {
-    function publishedCheckbox($post) {
-        return $post->published ?
-            "<input type='checkbox' id='chk{$post->id}' checked/><label for='chk{$post->id}'>YES</label>" :
-            "<input type='checkbox' id='chk{$post->id}'/><label for='chk{$post->id}'>NO</label>";
+if (!function_exists('checkbox')) {
+    function checkbox($checked, array $attributes = [])
+    {
+        $id = $attributes['name'] . $attributes['value'];
+        return sprintf('<input type="checkbox" %s %s %s/><label for="' .$id  . '"></label>',
+                        'id="' . $id . '"',
+                        domAttrGenerator($attributes),
+                        $checked ? 'checked' : ''
+                      );
+    }
+}
+
+if (!function_exists('domAttrGenerator')) {
+    function domAttrGenerator(array $attributes = [])
+    {
+        $attr = ' ';
+
+        foreach ($attributes as $key => $value) {
+            $attr .= $key . '="' . $value . '" ';
+        }
+        return $attr;
     }
 }
