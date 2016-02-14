1f0fe8d9e32e380f7570d98b34a35cdb70a8d81d
刘义 <forehalo@gmail.com>
2016-02-14 22:25:11 +0800

    update comments index layout

--- resources/views/back/comments/index.blade.php
+++ resources/views/back/comments/index.blade.php
@@ -8,50 +8,45 @@
         <h4 class="page-header">Comments</h4>
         <div class="divider"></div>
 
-        <div class="col s12">
-            <div class="row">
-                <div class="col s12">
-                    <div class="card-panel">
-                        <table class="bordered striped">
-                            <thead>
-                            <tr>
-                                <th>Author</th>
-                                <th>Date</th>
-                                <th>Post</th>
-                                <th>Valid</th>
-                                <th>Seen</th>
-                                <th>Operation</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($comments as $comment)
-                                <tr>
-                                    <td>{!! $comment->name !!}</td>
-                                    <td>{!! substr($comment->created_at, 0, 10) !!}</td>
-                                    <td>{!! $comment->post->title !!}</td>
-                                    <td>{!! checkbox($comment->valid, ['name' => 'valid', 'value' => $comment->id, 'class' => 'filled-in put-chk']) !!}</td>
-                                    <td>{!! checkbox($comment->seen, ['name' => 'seen', 'value' => $comment->id, 'class' => 'filled-in put-chk']) !!}</td>
-                                    <td>
-                                        {!! Form::open(['method' => 'delete', 'url' => '/comments/' . $comment->id]) !!}
-                                        {!! destroy('Really destroy this comment?') !!}
-                                        {!! Form::close() !!}
-                                    </td>
-                                </tr>
-                                <tr><td colspan="6">{!! $comment->content !!}</td></tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            {!! $links !!}
+        <div class="card-panel">
+            <table class="bordered striped">
+                <thead>
+                <tr>
+                    <th>Author</th>
+                    <th>Date</th>
+                    <th>Post</th>
+                    <th>Valid</th>
+                    <th>Seen</th>
+                    <th>Operation</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($comments as $comment)
+                    <tr>
+                        <td>{!! $comment->name !!}</td>
+                        <td>{!! substr($comment->created_at, 0, 10) !!}</td>
+                        <td>{!! $comment->post->title !!}</td>
+                        <td>{!! checkbox($comment->valid, ['name' => 'valid', 'value' => $comment->id, 'class' => 'filled-in put-chk']) !!}</td>
+                        <td>{!! checkbox($comment->seen, ['name' => 'seen', 'value' => $comment->id, 'class' => 'filled-in put-chk']) !!}</td>
+                        <td>
+                            {!! Form::open(['method' => 'delete', 'url' => '/comments/' . $comment->id]) !!}
+                            {!! destroy('Really destroy this comment?') !!}
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="6">{!! $comment->content !!}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
-    </div>
+        {!! $links !!}
 @stop
 @section('script')
-    <script>
-        @if(session('ok'))
-            Materialize.toast('{!! session('ok') !!}', 3000);
-        @endif
-    </script>
+   <script>
+   @if(session('ok'))
+       Materialize.toast('{!! session('ok') !!}', 3000);
+   @endif
+   </script>
 @stop
