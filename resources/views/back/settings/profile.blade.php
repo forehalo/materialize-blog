d5a4bdb6d5becc0c6b438de80c7ef0c1a73d476e
刘义 <forehalo@gmail.com>
2016-02-16 16:51:08 +0800

    update setting page layout

--- resources/views/back/settings/profile.blade.php
+++ resources/views/back/settings/profile.blade.php
@@ -4,8 +4,8 @@
         <div class="card-content">
             <span class="card-title">Basic</span>
             <div class="row">
-                {!! formText('s12', 'title', 'title', $errors, 'title', 'subtitles', true, $config['title']) !!}
-                {!! formText('s12', 'name', 'name', $errors, 'name', 'person', true, $config['name']) !!}
+                {!! formText('s12 l8', 'title', 'title', $errors, 'title', 'subtitles', true, $config['title']) !!}
+                {!! formText('s12 l8', 'name', 'name', $errors, 'name', 'person', true, $config['name']) !!}
             </div>
         </div>
     </div>
@@ -13,8 +13,8 @@
         <div class="card-content">
             <span class="card-title">Information card</span>
             <div class="row">
-                {!! formText('s12', 'card-title', 'card-title', $errors, 'card title', '', true, $config['card_title']) !!}
-                {!! formText('s12', 'notice', 'notice', $errors, 'card notice', '', true, $config['notice']) !!}
+                {!! formText('s12 l8', 'card-title', 'card-title', $errors, 'card title', '', true, $config['card_title']) !!}
+                {!! formText('s12 l8', 'notice', 'notice', $errors, 'card notice', '', true, $config['notice']) !!}
             </div>
         </div>
     </div>
@@ -22,10 +22,10 @@
         <div class="card-content">
             <span class="card-title">Site</span>
             <div class="row">
-                {!! formText('s12', 'description', 'description', $errors, 'site description', 'description', true, $config['description']) !!}
-                {!! formText('s12', 'url', 'url', $errors, 'url', 'web', true, $config['url']) !!}
-                {!! Form::button('update profile', ['type' => 'submit', 'class' => 'btn waves-effect']) !!}
+                {!! formText('s12 l8', 'description', 'description', $errors, 'site description', 'description', true, $config['description']) !!}
+                {!! formText('s12 l8', 'url', 'url', $errors, 'url', 'web', true, $config['url']) !!}
             </div>
+            {!! Form::button('update profile', ['type' => 'submit', 'class' => 'btn waves-effect']) !!}
         </div>
     </div>
     {!! Form::close() !!}
--- resources/views/back/settings/view.blade.php
+++ resources/views/back/settings/view.blade.php
@@ -4,9 +4,9 @@
         <div class="card-content">
             <span class="card-title">Pagination</span>
             <div class="row">
-                {!! formText('s12', 'post_per_page', 'post', $errors, 'Posts count per page', '', true, $config['post_per_page']) !!}
-                {!! formText('s12', 'post_per_page_admin', 'post_admin', $errors, 'Posts count per page in dashboard', '', true, $config['post_per_page_admin']) !!}
-                {!! formText('s12', 'comment_per_page_admin', 'comment', $errors, 'Comments count per page in dashboard', '', true, $config['comment_per_page_admin']) !!}
+                {!! formText('s12 l6', 'post_per_page', 'post', $errors, 'Posts count per page', '', true, $config['post_per_page']) !!}
+                {!! formText('s12 l6', 'post_per_page_admin', 'post_admin', $errors, 'Posts count per page in dashboard', '', true, $config['post_per_page_admin']) !!}
+                {!! formText('s12 l6', 'comment_per_page_admin', 'comment', $errors, 'Comments count per page in dashboard', '', true, $config['comment_per_page_admin']) !!}
             </div>
         </div>
     </div>
@@ -14,9 +14,9 @@
         <div class="card-content">
             <span class="card-title">Visibility</span>
             <div class="row">
-                {!! formText('s12', 'hot_tags_count', 'tags', $errors, 'Hot tags count shown', '', true, $config['hot_tags_count']) !!}
-                {!! Form::button('update view', ['type' => 'submit', 'class' => 'btn waves-effect']) !!}
+                {!! formText('s12 l6', 'hot_tags_count', 'tags', $errors, 'Hot tags count shown', '', true, $config['hot_tags_count']) !!}
             </div>
+            {!! Form::button('update view', ['type' => 'submit', 'class' => 'btn waves-effect']) !!}
         </div>
     </div>
     {!! Form::close() !!}
