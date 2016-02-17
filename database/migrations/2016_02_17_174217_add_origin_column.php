76562f4b8fed2d4b5a6911742cd1d128b929ff55
刘义 <forehalo@gmail.com>
2016-02-17 17:52:34 +0800

    add 'origin' column to put markdown text

---
+++ database/migrations/2016_02_17_174217_add_origin_column.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOriginColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->text('origin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('origin');
+        });
+    }
+}
