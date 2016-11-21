e70a6e404d66575cda1ccc7c3a6737b27eceb7e2
forehalo <forehalo@gmail.com>
2016-11-21 16:04:10 +0800

    use collection to show posts

---
+++ resources/assets/js/components/circular-loader.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="preloader-wrapper active" :class="size">
+        <div class="spinner-layer spinner-blue">
+            <div class="circle-clipper left">
+                <div class="circle"></div>
+            </div>
+            <div class="gap-patch">
+                <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+        </div>
+
+        <div class="spinner-layer spinner-red">
+            <div class="circle-clipper left">
+                <div class="circle"></div>
+            </div>
+            <div class="gap-patch">
+                <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+        </div>
+
+        <div class="spinner-layer spinner-yellow">
+            <div class="circle-clipper left">
+                <div class="circle"></div>
+            </div>
+            <div class="gap-patch">
+                <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+        </div>
+
+        <div class="spinner-layer spinner-green">
+            <div class="circle-clipper left">
+                <div class="circle"></div>
+            </div>
+            <div class="gap-patch">
+                <div class="circle"></div>
+            </div>
+            <div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            size: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
--- resources/assets/js/components/posts.vue
+++ resources/assets/js/components/posts.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="row">
         <div class="col s12">
-            <div class="card post-card" v-for="post in posts">
-                <div class="card-content">
-                    <span class="card-title">{{ post.title }}</span>
+            <div class="collection z-depth-1 posts">
+                <div class="collection-item post-item" v-for="post in posts">
+                    <h5 class="title">
+                        <router-link to="#">{{ post.title }}</router-link>
+                    </h5>
                     <div class="label">
                         published at {{ post.created_at.substr(0, 10) }}
                         <i class="material-icons">visibility</i>{{ post.view_count }}
@@ -14,24 +16,49 @@
                         </a>
                     </div>
                     <p class="summary">{{ post.summary }}</p>
-                    <div class="row article-card no-margin">
+                    <div class="row no-margin">
                         <div class="col s12 m12 l12 no-padding">
                             <div class="chip" v-for="tag in post.tags">{{ tag.name }}</div>
                         </div>
                     </div>
                 </div>
             </div>
+            <!--<ul class="collection">
+                
+            </ul>
+            <div class="card post-card" v-for="post in posts">
+                <div class="card-content">
+                    
+                </div>
+            </div>-->
+
+            <!-- loader -->
+            <div class="loader-wrapper center">
+                <circular-loader v-show="loading"></circular-loader>
+            </div>
+            <!-- No more posts -->
+            <div class="card" v-if="!hasMore">
+                <div class="card-content">
+                    <div class="card-title">No More Posts</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
+    import CircularLoader from './circular-loader.vue';
     export default {
+
+        components: {
+            CircularLoader
+        },
 
     	data() {
     		return {
                 page: 0,
     			posts: [],
                 hasMore: true,
+                loading: false,
     		}
     	},
 
@@ -41,13 +68,22 @@
 
         methods: {
             loadMorePosts() {
-                if(this.hasMore) {
-                    this.$http.get('api/posts', {page: this.page + 1}).then((response) => {
-                        this.posts = this.posts.concat(response.body.data);
-                        this.page = response.body.current_page;
-                        this.hasMore = response.next_page_url != null;
+                if(this.hasMore && !this.loading) {
+                    this.loading = true;
+                    this.$http.get(`api/posts?page=${this.page + 1}`).then((response) => {
+                        let body = response.body;
+                        this.posts = this.posts.concat(body.data);
+                        this.page = body.current_page;
+                        this.hasMore = body.next_page_url != null;
+                        Materialize.scrollFire([{
+                            selector: 'footer', 
+                            offset: -100,
+                            callback: this.loadMorePosts
+                        }]);
+                        this.loading = false;                      
                     }, (response) => {
                         // TODO
+                        this.loading = false;                        
                     });
                 }
             }
