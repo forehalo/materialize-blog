4715fddbf32869d040085cc138300ccbd30d60e2
forehalo <forehalo@gmail.com>
2016-12-14 15:26:50 +0800

    add laravel tracy for debugging

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
         //
 
         /*
---
+++ config/tracy.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'enabled' => env('APP_DEBUG') === true,
+    'showBar' => env('APP_ENV') !== 'production',
+    'accepts' => [
+        'text/html',
+    ],
+    'editor' => 'subl://open?url=file://%file&line=%line',
+    'maxDepth' => 4,
+    'maxLength' => 1000,
+    'scream' => true,
+    'showLocation' => true,
+    'strictMode' => true,
+    'editorMapping' => [],
+    'panels' => [
+        'routing' => true,
+        'database' => true,
+        'view' => true,
+        'event' => false,
+        'session' => true,
+        'request' => true,
+        'auth' => true,
+        'html-validator' => false,
+        'terminal' => false,
+    ],
+];
