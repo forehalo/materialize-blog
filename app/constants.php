702d939c1c500c3a4479139fc919c4a570f78755
forehalo <forehalo@gmail.com>
2016-12-16 10:39:26 +0800

    add result api response code constants

--- app/constants.php
+++ app/constants.php
@@ -1,6 +1,17 @@
 <?php
 // Constants file
 
+// Restful api response http code.
+define('REST_GET_SUCCESS', 200);
+define('REST_CREATE_SUCCESS', 201);
+define('REST_UPDATE_SUCCESS', 200);
+define('REST_DELETE_SUCCESS', 204);
+define('REST_NOT_MODIFIED', 304);
+define('REST_BAD_REQUEST', 400);
+define('REST_INVALID_FIELDS', 422);
+define('REST_RESOURCE_NOT_FOUND', 404);
+define('REST_REQUEST_LIMIT', 429);
+
 // Post
 define('POST_NOT_FOUND', 1001);
 define('FAIL_TO_LIKE_POST', 1002);
