db982126f3e12dae1260498a3a15165b25ee492a
forehalo <forehalo@gmail.com>
2016-12-19 21:11:24 +0800

    add meta information

--- config/blog.php
+++ config/blog.php
@@ -11,7 +11,7 @@ return [
     'keywords' => '',
 
     // Author
-    'anthor' => '',
+    'author' => '',
 
     'logo' => '// Forehalo'
 ];
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="{{ config('blog.author') }}">
+    <meta name="description" content="{{ config('blog.desc') }}">
+    <meta name="keywords" content="{{ config('blog.keywords') }}">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset(elixir('css/app.css')) }}">
     <title>{{ config('blog.title') }}</title>
