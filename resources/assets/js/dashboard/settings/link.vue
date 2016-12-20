7e28bb1ad2ce0ef178fd689f968e7f8436ac71d4
forehalo <forehalo@gmail.com>
2016-12-20 15:45:27 +0800

    finish links management

--- resources/assets/js/dashboard/settings/link.vue
+++ resources/assets/js/dashboard/settings/link.vue
@@ -4,7 +4,7 @@
             <div class="card-content">
                 <span class="card-title">Friend links</span>
                 <label>click to edit</label>
-                <a href="javascript:;" class="btn btn-floating btn-warning waves-effect right" @click="openModal">
+                <a href="javascript:;" class="btn btn-floating btn-warning waves-effect right" @click="openCreateModal">
                     <i class="material-icons">add</i>
                 </a>
                 <div class="loader-wrapper center" v-if="loading">
@@ -16,7 +16,7 @@
                     </p>
 
                     <div class="collection" v-else>
-                        <a href="javascript:;" class="collection-item" v-for="link in links" >
+                        <a href="javascript:;" class="collection-item" v-for="(link, index) in links" @click="openEditModal(link, index)">
                             <div>
                                 <span class="title">{{ link.name }}</span>
                                 <p>{{ link.link }}</p>
@@ -28,8 +28,8 @@
             </div>
             <div class="modal" id="new-link-modal">
                 <div class="modal-content">
-                    <h4>New friend link</h4>
-                    <form @submit.prevent="addNewLink">
+                    <h4 class="capitalize">{{ modalType }}</h4>
+                    <form @submit.prevent="updateOrCreate">
                         <div class="input-field col s12">
                             <i class="material-icons prefix">face</i>
                             <input type="text" name="name" id="name" class="validate" v-model="form.name">
@@ -40,8 +40,21 @@
                             <input type="text" name="website" id="website" class="validate" v-model="form.link">
                             <label for="website">Website</label>
                         </div>
-                        <button type="submit" class="btn waves-effect btn-success">Save</button>
+                        <button type="submit" class="btn waves-effect btn-success">{{ modalType }}</button>
+                        <a href="javascript:;" class="btn btn-danger waves-effect" v-show="modalType === 'update'" @click="wantDestroy">Destroy</a>
                     </form>
+                </div>
+            </div>
+            <div class="modal bottom-sheet" id="destroy-modal">
+                <div class="modal-content">
+                    <h4>Warning</h4>
+                    <p>Really want to destroy this link? You can't recover it after done!</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="javascript:;" class="left modal-action waves-effect waves-red btn-flat"
+                       @click="destroy">Confirm</a>
+                    <a href="javascript:;"
+                       class="left modal-action modal-close waves-effect waves-green btn-flat">Cancel</a>
                 </div>
             </div>
         </div>
@@ -55,18 +68,26 @@
         },
         data() {
             return {
-                modal: null,
                 loading: true,
                 links: [],
+                modal: null,
+                modalType: 'create',
+                editIndex: -1,
                 form: {
                     name: '',
                     link: ''
-                }
+                },
+                willDestroyed: null
             }
         },
         mounted() {
             this.fetchLinks();
-            this.modal = $('#new-link-modal').modal();
+            this.modal = $('#new-link-modal').modal({
+                ready: () => {
+                    Materialize.updateTextFields();
+                }
+            });
+            $('#destroy-modal').modal();
         },
         methods: {
             fetchLinks() {
@@ -80,18 +101,62 @@
                             this.lading = false;
                         });
             },
-            openModal() {
+            openCreateModal() {
+                this.modalType = 'create';
+                this.form = {name: '', link: ''};
                 this.modal.modal('open');
             },
-            addNewLink() {
+            openEditModal(link, index) {
+                this.modalType = 'update';
+                this.form = link;
+                this.editIndex = index;
+                this.modal.modal('open');
+            },
+            updateOrCreate() {
+                if (this.modalType === 'update') {
+                    this.updateLink();
+                } else if(this.modalType === 'create') {
+                    this.createLink();
+                }
+            },
+            createLink() {
+                this.modal.modal('close');
                 store.loading = true;
                 this.$http.post('/api/dashboard/settings/links', this.form)
                         .then(response => {
-                            this.modal.modal('close');
                             store.loading = false;
                             this.links.push(response.body);
                         }, response => {
                             store.loading = false;
+                        });
+            },
+            updateLink() {
+                this.modal.modal('close');
+                store.loading = true;
+                this.$http.put(`/api/dashboard/settings/links/${this.form.id}`, this.form)
+                        .then(response => {
+                            store.loading = false;
+                            this.links[this.editIndex] = this.form;
+                        }, response => {
+                            store.loading = false;
+                        });
+            },
+            wantDestroy() {
+                this.modal.modal('close');
+                this.willDestroyed = {link: this.form, index: this.editIndex};
+                $('#destroy-modal').modal('open');
+            },
+            destroy() {
+                store.loading = true;
+                $('#destroy-modal').modal('close');
+                this.$http.delete(`/api/dashboard/settings/links/${this.willDestroyed.link.id}`)
+                        .then(response => {
+                            this.links.splice(this.willDestroyed.index, 1);
+                            store.loading = false;
+                            Materialize.toast('Delete friend link successfully.', 4000);
+                        }, response => {
+                            store.loading = false;
+                            Materialize.toast('Delete friend link failed.', 4000);
                         });
             }
         }
