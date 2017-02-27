efe0259c875db7ad8cf6614098428415a61f4752
forehalo <forehalo@gmail.com>
2017-02-27 22:41:28 +0800

    dont seed mock data default

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(AdminTableSeeder::class);
-        $this->call(BlogSeeder::class);
         $this->call(SettingTableSeeder::class);
     }
 }
